27b204f49fbc74089b45df4ae8a62b0c295decd9
Florian Mold <florian.mold@agnet.at>
2022-01-05 16:58:38 +0100

    feat(server/service): implement the greeting-service from the interface

---
+++ server/src/main/java/at/agnet/server/service/GreetingServiceImpl.java
@@ -0,0 +1,28 @@
+package at.agnet.server.service;
+
+import at.agnet.server.GreetingRequest;
+import at.agnet.server.GreetingResponse;
+import at.agnet.server.GreetingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+// similar to a rest controller.
+@GrpcService // Helps us identifying so that spring boot finds this as a service.
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+
+    @Override
+    public void greeting(GreetingRequest request, StreamObserver<GreetingResponse> responseObserver) {
+        var message = request.getMessage();
+        System.out.println("Received Message: " + message);
+
+        // a builder is created
+        var greetingResponse = GreetingResponse.newBuilder()
+                .setMessage("Received your " + message + ". Hello from Server.")
+                .build();
+
+        responseObserver.onNext(greetingResponse);
+
+        // on-complete closes the connection
+        responseObserver.onCompleted();
+    }
+}
